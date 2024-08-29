2648c31ce855e73f814de802bd5ed01d51c4d459
Alishka fernandes <135259057+AlishkaFernandes@users.noreply.github.com>
2024-08-29 13:40:04 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,22 +1,22 @@
-pipeline{
+pipeline {
     agent any
 
-    stages{
-        stage("clone the repo"){
-            steps{
-               bat 'git clone "https://github.com/AlishkaFernandes/Jenkins-pipeline.git"'
+    stages {
+        stage("Clone the Repo") {
+            steps {
+                bat 'git clone "https://github.com/AlishkaFernandes/Jenkins-pipeline.git"'
             }
         }
 
-        stage("Build a Docker Image"){
-            steps{
-               bat 'docker build -t  testImage  .'
+        stage("Build a Docker Image") {
+            steps {
+                bat 'docker build -t testImage ./Jenkins-pipeline'
             }
         }
 
-        stage("Run Docker Container"){
-            steps{
-                bat 'docker run testImage'
+        stage("Run Docker Container") {
+            steps {
+                bat 'docker run -d testImage'
             }
         }
     }
